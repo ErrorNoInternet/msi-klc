6912dea24faa3064ad1dc857ab520bb700c60f89
ErrorNoInternet <error.nointernet@gmail.com>
2022-09-25 15:13:11 +0800

    Add RGB wave animation

--- animations/generator.py
+++ animations/generator.py
@@ -22,3 +22,20 @@ for i in reversed(range(0, 255)):
 for i in reversed(range(0, 255)):
     file.write(f"color:{i};{i};{i}, region:right\nsleep:5\n")
 file.close()
+
+file = open("rgb-wave.txt", "w+")
+file.write("loop_forever\n\n")
+for i in range(0, 255):
+    file.write(f"color:{i};0;0, region:left\nsleep:5\n")
+
+for i in reversed(range(0, 255)):
+    file.write(f"color:{i};0;0, region:left\nsleep:5\n")
+    file.write(f"color:0;{255-i};0, region:middle\nsleep:5\n")
+
+for i in reversed(range(0, 255)):
+    file.write(f"color:0;{i};0, region:middle\nsleep:5\n")
+    file.write(f"color:0;0;{255-i}, region:right\nsleep:5\n")
+
+for i in reversed(range(0, 255)):
+    file.write(f"color:0;0;{i}, region:right\nsleep:5\n")
+file.close()
