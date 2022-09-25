decf3d9a2cd4c4bb278c887a94b8b4d4e7c1e55e
ErrorNoInternet <error.nointernet@gmail.com>
2022-09-25 14:50:39 +0800

    Update generator

--- animations/generator.py
+++ animations/generator.py
@@ -1,6 +1,24 @@
 file = open("breathe.txt", "w+")
+file.write("loop_forever\n")
 for i in range(0, 255):
     file.write(f"color:{i};{i};{i}\nsleep:10\n")
-for i in reversed(list(range(0, 255))):
+for i in reversed(range(0, 255)):
     file.write(f"color:{i};{i};{i}\nsleep:10\n")
+file.close()
+
+file = open("wave.txt", "w+")
+file.write("loop_forever\n")
+for i in range(0, 255):
+    file.write(f"color:{i};{i};{i}, region:left\nsleep:5\n")
+
+for i in reversed(range(0, 255)):
+    file.write(f"color:{i};{i};{i}, region:left\nsleep:5\n")
+    file.write(f"color:{255-i};{255-i};{255-i}, region:middle\nsleep:5\n")
+
+for i in reversed(range(0, 255)):
+    file.write(f"color:{i};{i};{i}, region:middle\nsleep:5\n")
+    file.write(f"color:{255-i};{255-i};{255-i}, region:right\nsleep:5\n")
+
+for i in reversed(range(0, 255)):
+    file.write(f"color:{i};{i};{i}, region:right\nsleep:5\n")
 file.close()
