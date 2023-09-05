7db8a8ed314fe9f6a871a84d336a50d4d12b1821
ErrorNoInternet <errornointernet@envs.net>
2023-09-05 23:21:19 +0800

    Add Nix dependencies

---
+++ shell.nix
@@ -0,0 +1,25 @@
+with import <nixpkgs> {};
+
+let
+    mozillaOverlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+    pkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
+    channel = pkgs.rustChannelOf {
+        date = "2023-09-01";
+        channel = "nightly";
+    };
+    rust = (channel.rust.override {
+        targets = [ "x86_64-unknown-linux-musl" ];
+        extensions = [ "rust-src" ];
+    });
+in pkgs.mkShell {
+    name = "rust-env";
+    buildInputs = [
+        rust
+        pkgs.pkg-config
+        pkgs.udev
+    ];
+
+    PKG_CONFIG_ALLOW_CROSS = true;
+    PKG_CONFIG_ALL_STATIC = true;
+    LIBZ_SYS_STATIC = 1;
+}
